55ac3e19c7e5d51f5112cbdb6a2925bc1396b4c5
Joy Chen <joychen5069@gmail.com>
2020-04-30 20:49:26 -0400

    fixed apiRoutes so that the id logged a number

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -4,55 +4,55 @@ const path = require("path")
 
 module.exports = (app) => {
     //get the notes already stored
-  app.get('/api/notes', (req, res) => {
-      console.log(notesData)
-    res.json(notesData);
-  });
-
-  //post the notes after they've been submitted
-  app.post('/api/notes', (req, res) => {
-    const notes = req.body;
+    app.get('/api/notes', (req, res) => {
         console.log(notesData)
-   
-      notesData.push(notes);
-      fs.readFile(path.join(__dirname,'../db/db.json'),"utf8",function(error, notesData) {
-          if(error) {
-              console.log(error)
-            
-          }
+        res.json(notesData);
+    });
+
+    //post the notes after they've been submitted
+    app.post('/api/notes', (req, res) => {
+        const notes = req.body;
+        console.log(notesData)
+
         if (notesData.length === 0) {
             notes.id = 1
         }
         else {
-        notes.id = notesData[notesData.length -1].id + 1
+            notes.id = notesData[notesData.length - 1].id + 1
         }
 
-        fs.writeFile(path.join(__dirname,'../db/db.json'), notesData, function(error) {
-            if(error) {
+        fs.readFile(path.join(__dirname, '../db/db.json'), "utf8", function (error, notesData) {
+            if (error) {
+                console.log(error)
+            }
+
+
+            fs.writeFile(path.join(__dirname, '../db/db.json'), notesData, "utf8", function (error) {
+                if (error) {
+                    console.log(error)
+                }
+            }
+            )
+
+        });
+
+        notesData.push(notes);
+        res.json(true);
+
+    });
+
+    app.delete('/api/notes/:id', (req, res) => {
+        const notes = req.params.id;
+        console.log(notes)
+        const foundIndex = notesData.findIndex((el) => el.id = notes)
+        notesData.splice(foundIndex, 1)
+        fs.writeFile(path.join(__dirname, '../db/db.json'), notesData, function (error) {
+            if (error) {
                 console.log(error)
             }
         }
         )
+        res.json(true);
+    });
 
-      });
-      
-      
-      res.json(true);
-    
-  });
-
-  app.delete('/api/notes/:id', (req, res) => {
-    const notes = req.params.id;
-        console.log(notes)
-    const foundIndex = notesData.findIndex((el) => el.id = notes)
-    notesData.splice(foundIndex, 1)
-    fs.writeFile(path.join(__dirname,'../db/db.json'), notesData, function(error) {
-        if(error) {
-            console.log(error)
-        }
-    }
-    )
-      res.json(true);
-  });
-
-  };
+};
